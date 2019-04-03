6130dee9a1790ae18aaa4c700a04d857341ba01e
Brian Vallelunga <vallelungabrian@gmail.com>
2019-04-03 11:22:47 -0700

    updated cli for error commands

--- controllers/help.js
+++ controllers/help.js
@@ -1,7 +1,11 @@
+const chalk = require("chalk")
+
+
 module.exports = function(program) {
 
-  program.on("command:*", function () {
-    program.outputHelp()
+  program.on("command:*", function (args) {
+    console.log(chalk.bold.red("Command not found: "), args.join(" "))
+    console.log(chalk.bold.blue("Use \"doppler --help\" to see all available commands."))
     process.exit(1)
   });
   
