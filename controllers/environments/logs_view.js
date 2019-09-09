f0377a7a4a3a19083c5d994593a069dcc8292624
Brian Vallelunga <vallelungabrian@gmail.com>
2019-09-08 19:02:59 -0700

    Merging Logs View and Logs Diff

--- controllers/environments/logs_view.js
+++ controllers/environments/logs_view.js
@@ -1,24 +1,43 @@
 const striptags = require("striptags")
 const moment = require("dayjs")
-const fs = require("fs")
 const chalk = require("chalk")
 
 
 function task_runner(program, options) {
   program.api.environments.logs_view({
-    environment: options.environment,
-    log: options.log
+    log: options.log,
+    environment: options.environment
   }).then(function(response) {
     if(options.json) {
       console.log(JSON.stringify(response))
     } else {
-      const log = response.log
-      console.log([
+      const { log } = response
+
+      const diff = log.diff.sort(function(a, b) {
+        const a_value = a.name || a[lookup] || a.added || a.removed
+        const b_value = b.name || b[lookup] || a.added || a.removed
+        return a_value.localeCompare(b_value)
+      }).map(function(change) {
+        let added_body = change.added || ""
+        let removed_body = change.removed || ""
+
+        if(change.name !== undefined) {
+          added_body = `${change.name} = "${added_body}"`
+          removed_body = `${change.name} = "${removed_body}"`
+        }
+
+        return `${chalk.redBright(`- ${removed_body}`)}\n${chalk.greenBright(`+ ${added_body}`)}`
+      }).join("\n\n")
+
+      const text = [
         chalk.bold.yellow(`Log: ${log.id}`),
         `Actor: ${log.user.name} <${log.user.email}>`,
         `Date: ${moment(log.created_at).format("ddd MMM d H:MA")}`,
-        `\n\t${striptags(log.text)}\n\n`
-      ].join("\n"))
+        `\n\t${striptags(log.text)}\n\n`,
+        diff
+      ].join("\n")
+
+      program.utils.scrollPrint(`Log: ${log.id}`, text)
     }
   })
 }
@@ -27,7 +46,7 @@ function task_runner(program, options) {
 module.exports = function(program) {
   program
     .command("environments:logs:view")
-    .description("specific environment audit log")
+    .description("view environment audit log diff")
     .option("-l, --log <id>", "log id")
     .option("-e, --environment <name>", "environment name")
     .option("--json", "print in json format", false)
